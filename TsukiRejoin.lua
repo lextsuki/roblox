48f780615362eccbf9a3259f6f9806648483774b
lextsuki <144962009+lextsuki@users.noreply.github.com>
2023-09-14 07:52:08 +0800

    Update TsukiRejoin.lua

--- TsukiRejoin.lua
+++ TsukiRejoin.lua
@@ -1,28 +1,18 @@
 -- The game ID you want to join
 local gameId = "8304191830"  -- Replace with the actual game ID you want to join
 
--- Send a notification when the script starts
-game.StarterGui:SetCore("SendNotification", {
-    Title = "Anti-DC-Rejoin",
-    Text = "Script Made by Tsuki",
-    Duration = 15
-})
-
--- Wait for a short time before continuing
-wait(1.0)
-
 -- Function to check if the player is on a disconnection page
 function isOnDisconnectedPage()
-    local gui = game.Players.LocalPlayer:FindFirstChild("PlayerGui")
-    if gui then
-        local errorFrame = gui:FindFirstChild("ErrorFrame")
+    local playerGui = game.Players.LocalPlayer:FindFirstChild("PlayerGui")
+    if playerGui then
+        local errorFrame = playerGui:FindFirstChild("ErrorFrame")
         if errorFrame then
-            local errorMessage = errorFrame:FindFirstChild("ErrorLabel")
-            if errorMessage and errorMessage:IsA("TextLabel") then
+            local errorLabel = errorFrame:FindFirstChild("ErrorLabel")
+            if errorLabel and errorLabel:IsA("TextLabel") then
                 -- Check if the error message contains keywords like "Disconnected" or specific error codes
-                local errorTextValue = errorMessage.Text:lower()
-                if errorTextValue:match("disconnected") or errorTextValue:match("error 277")
-                    or errorTextValue:match("error 279") or errorTextValue:match("failed to connect to the game") then
+                local errorText = errorLabel.Text:lower()
+                if errorText:match("disconnected") or errorText:match("error 277")
+                    or errorText:match("error 279") or errorText:match("failed to connect to the game") then
                     return true
                 end
             end
@@ -33,17 +23,42 @@ end
 
 -- Function to join the specified game
 function joinGame()
-    game:GetService("TeleportService"):TeleportToPlaceInstance(gameId, game.Players.LocalPlayer.UserId)
-end
-
--- Check for disconnection every 10 seconds
-while true do
-    wait(10)
-    if isOnDisconnectedPage() then
-        joinGame()
+    local teleportService = game:GetService("TeleportService")
+    local success, errorMessage = pcall(function()
+        teleportService:TeleportToPlaceInstance(gameId, game.Players.LocalPlayer.UserId)
+    end)
+    
+    if not success then
+        warn("Failed to teleport to the game: " .. errorMessage)
     end
 end
 
--- The following line always executes regardless of disconnection status
--- Ensure this URL is correct and safe to use
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TrapstarKSSKSKSKKS/Main/main/TrapHub.lua"))()
+-- Send the notification when the script starts
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Anti-DC-Rejoin",
+    Text = "Script Made by Tsuki",
+    Duration = 15
+})
+
+-- Rate limiting parameters
+local maxRetries = 3
+local retries = 0
+local retryDelay = 5  -- Delay in seconds between retries
+
+-- Check for disconnection every 10 seconds
+while true do
+    print("Checking")  -- Print a message to indicate the check is happening
+    wait(10)
+    if isOnDisconnectedPage() then
+        -- Retry the rejoin with a delay if a disconnection is detected
+        while retries < maxRetries do
+            joinGame()
+            wait(retryDelay)  -- Wait for the specified delay before retrying
+            if not isOnDisconnectedPage() then
+                break
+            end
+            retries = retries + 1
+        end
+        retries = 0  -- Reset the retry counter after a successful rejoin
+    end
+end
